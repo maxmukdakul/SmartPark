c37507065532fe1b1b77b61eb17553fcb925dec2
max <maxza1408>
2026-05-09 12:42:07 +0700

    Initial Commit

---
+++ SmartPark/SmartParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartParkApp.swift
+//  SmartPark
+//
+//  Created by Max on 9/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct SmartParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
